d3ebc4461e2ce80397c1c3d0ca3e9bc5f18342b7
Flavio Pocari <59371153+flaviopopoff@users.noreply.github.com>
2020-07-04 17:46:12 +0200

    Update Oracle1_solution.py

--- CyberChallenge/Week 06/Oracle1_solution.py
+++ CyberChallenge/Week 06/Oracle1_solution.py
@@ -1,23 +1,35 @@
+from pwn import *
 from Crypto.Util.number import *
 from gmpy2 import c_div
-from pwn import *
 
 # decrypt(encrypt(FLAG) * encrypt(2)) == 2*FLAG
-HOST = "oracle1.challs.cyberchallenge.it"
-PORT = 7011
-r = remote(HOST, PORT)
+
+r = remote('149.202.200.158', 7011)
 
 r.recvuntil('flag: ')
 flag_encrypted = r.recvline().decode()[0:-1]
-r.sendlineafter('> ', str('1'))
-r.sendlineafter('Plaintext > ', str('2'))
+
+
+r.recvuntil('> ')
+r.sendline(str('1').encode())
+
+r.recvuntil('Plaintext > ')
+r.sendline(str('2').encode())
+
 r.recvuntil('Encrypted: ')
 two_encrypted = r.recvline().decode()[0:-1]
-flag_two_encrypt = int(flag_encrypted) * int(two_encrypted)
-r.sendlineafter('> ', str('2'))
-r.sendlineafter('Ciphertext > ', str(flag_two_encrypt))
-r.recvuntil('Decrypted: ')
 
+flag_two_encrypt = int(flag_encrypted) * int(two_encrypted)
+
+
+r.recvuntil('> ')
+r.sendline(str('2').encode())
+
+r.recvuntil('Ciphertext > ')
+r.sendline(str(flag_two_encrypt).encode())
+
+r.recvuntil('Decrypted: ')
 flag_two_decrypt = r.recvline().decode()[0:-1]
+
 encrypted_flag = c_div(int(flag_two_decrypt), 2)
 print('Flag:', long_to_bytes(encrypted_flag).decode())
