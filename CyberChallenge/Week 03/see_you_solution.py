08d2515fabbd27457cb07e33e88f00e2bbd755f0
Flavio Pocari <59371153+flaviopopoff@users.noreply.github.com>
2020-07-04 19:05:33 +0200

    Update see_you_solution.py

--- CyberChallenge/Week 03/see_you_solution.py
+++ CyberChallenge/Week 03/see_you_solution.py
@@ -31,19 +31,26 @@ def encrypt(clear, key):
 start = time.time()
 
 print("Find all key.")
-key_table = [''.join(_) for _ in product(string.ascii_lowercase, repeat=4)]
+# Riempio una lista con tutte le combinazioni possibili di lunghezza 4 che posso fare con le lettere a...z (aaaa, abbb, abca, ahca..).
+key_table = [''.join(_) for _ in product(string.ascii_lowercase, repeat=4)] 
 
 print("Fill the encrypt table.")
+# Riempio la lista con tutti messaggi 'm' cifrati da tutte le key presenti nella lista.
 encrypt_table = [encrypt(m, key_one) for key_one in key_table]
 
 print("Fill the decrypt table.")
+# Riempio la lista con tutti messaggi 'c' decifrati da tutte le key presenti nella lista.
 decrypt_table = [decrypt(c, key_due) for key_due in key_table]
 
 print("Searching the flag.")
-intersect = set(encrypt_table).intersection(set(decrypt_table))
+# Interseco le due liste e ritorno gli elementi in comune in una collection, ovvero encrypt(m) == decrypt(c).
+intersect = set(encrypt_table).intersection(set(decrypt_table))  #{'SEtUFW5VZBVhR2NaZwZYYxVaV1oVSVhpbgZSWmNaVGc='}
 
+# Con 'encrypt_table.index(_in)' ritorno l'indice in cui si trova l'elemento della collection che corrisponde alla key con cui è stato effettuato l'encrypt.
+# Vale lo stesso ragionamento per l'altra lista.
 for _in in intersect:
-    print("Flag: CCIT{%s}" % (key_table[encrypt_table.index(_in)] + key_table[decrypt_table.index(_in)]))
+    # key_table[encrypt_table.index(_in)] -> ufou, key_table[decrypt_table.index(_in)] -> ndit.
+    print("Flag: CCIT{%s}" % (key_table[encrypt_table.index(_in)] + key_table[decrypt_table.index(_in)])) # CCIT{ufoundit}
 
 end = time.time()
 print(f"Tempo impiegato: {end - start} sec.")
