b8af40f663acf635f1e68739a16ba61cd4394726
Th3F0x-code <alessio.ciccola@gmail.com>
2020-08-04 13:52:35 +0200

    New Writeups

---
+++ H@cktivityCon/A E S T H E T I C/solution.py
@@ -0,0 +1,24 @@
+import re
+from string import ascii_lowercase, digits
+
+from pwn import *
+
+s = remote('jh2i.com', 50009)
+s.recvuntil("> ")
+
+flag = ''
+
+for i in range(1, 33):
+    s.sendline('a' * (32 - i))
+    base_block = re.findall('[a-f0-9]{64}', s.recvuntil("> ").decode('utf-8'))[0][:64]
+    for c in '_{}' + ascii_lowercase + digits:
+        s.sendline('a' * (32 - i) + flag + c)
+        block = re.findall('[a-f0-9]{128}', s.recvuntil("> ").decode('utf-8'))[0][:64]
+        if block == base_block:
+            flag = flag + c
+            print(flag)
+            break
+
+s.close()
+
+# FLAG --> flag{aes_that_ick_ecb_mode_lolz}
