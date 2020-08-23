58a464fe7a23c8cc58ac68fa513e466f48d86bcd
Th3F0x-code <alessio.ciccola@gmail.com>
2020-08-23 22:52:25 +0200

    New Writeups

---
+++ ASIS CTF Quals 2020/Elliptic Curve/solution.py
@@ -0,0 +1,57 @@
+import os
+os.environ["PWNLIB_NOTERM"] = "True"
+
+import hashlib
+import string
+import random
+from pwn import *
+
+IP = "76.74.178.201"
+PORT = 9531
+r = remote(IP, PORT, level="debug")
+POW = r.recvline().decode().split()
+x_len = int(POW[-1])
+suffix = POW[-5]
+hash_type = POW[-7].split('(')[0]
+
+"""
+The server asks for a random length string, hashed with a random hash
+function such that the last 3 bytes of the hash match a given prefix.
+"""
+while True:
+    X = ''.join(random.choices(string.ascii_letters + string.digits, k=x_len))
+    h = getattr(hashlib, hash_type)(X.encode()).hexdigest()
+    if h.endswith(suffix):
+        print(h)
+        break
+
+r.sendline(X)
+
+header = r.recvuntil(b'One of such points')
+
+points = r.recvline().split(b'P = (')[-1]
+points = points.split(b', ')
+px = Integer(points[0])
+py = Integer(points[-1][:-2])
+
+scale_data = r.recvline().split(b' ')
+scale = Integer(scale_data[3])
+
+p = px + 1
+assert p.is_prime()
+a = -1
+b = (py^2 - px^3 - a*px) % p
+E = EllipticCurve(GF(p), [a,b])
+P = E(px,py)
+
+Q = P*scale
+
+"""
+For some reason sending str(Q.xy()) to the server caused an error, so I 
+just switched to interactive and sent it myself. I'm sure it's a dumb
+formatting bug, but with the annoying POW to deal with, I can't be bothered
+to figure it out...
+"""
+# r.sendline(str(Q.xy()))
+print(Q.xy())
+r.interactive()
---
+++ ASIS CTF Quals 2020/Hadamard/solution.py
@@ -0,0 +1,97 @@
+from pwn import *
+from z3 import *
+from hashlib import *
+
+def matmul(A, B):
+
+	result = [[0]*len(A[0])]*len(A)
+
+		# iterating by row of A 
+	for i in range(len(A)): 
+	  
+		# iterating by coloum by B  
+		for j in range(len(B[0])): 
+	  
+			# iterating by rows of B 
+			for k in range(len(B)): 
+				result[i][j] += A[i][k] * B[k][j] 
+
+	return result
+
+
+def transpose(A): 
+	B = A[:][:]
+
+	for i in range(len(A)): 
+		for j in range(len(A[0])): 
+			B[i][j] = A[j][i]
+
+	return B
+
+
+p = remote('76.74.178.201', 8001)
+
+log.info(p.recv().decode())
+log.info(p.recvuntil('------------------------------------------------------------------------\n').decode())
+
+while True:
+	log.info(p.recvline().decode())
+	
+	data = p.recvuntil('|').decode().strip()[:-1].strip()
+
+	log.info(p.recv())
+
+	log.info(data)
+	mat = data.split('\n')
+
+	A = []
+	i = 0
+	j = 0
+
+	s = Solver()
+
+	for row in mat:
+		d = row.replace('[', '').replace(']', '')
+		d = d.split(',')
+		A.append([])
+
+		j = 0
+		for num in d:
+			if '?' in num:
+				A[i].append(Int(f'M_{i}_{j}'))
+				s.add(Or(A[i][j] == 1, A[i][j] == -1))
+			else:
+				A[i].append(int(num))
+			j += 1
+		i += 1
+
+	
+	for i in range(0, len(A)-1):
+		
+		for k in range(i+1, len(A)):
+			cond = None
+			for j in range(len(A[0])):
+				if cond == None:
+					cond = A[i][j] * A[k][j]
+				else:
+					cond += A[i][j] * A[k][j]
+
+			s.add(cond == 0)
+
+	print(s.check())
+	if s.check() == sat:
+		m = s.model()
+
+		for i in range(len(A)):
+			for j in range(len(A[0])):
+				if not isinstance(A[i][j], int):
+					A[i][j] = m[A[i][j]]
+
+		print(A)
+
+		p.sendline(md5(str(A).encode()).hexdigest())
+
+		log.info(p.recvline())
+
+	else:
+		print('Nothing Here')
